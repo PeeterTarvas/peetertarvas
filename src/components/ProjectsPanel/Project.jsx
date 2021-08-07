5aa7c2c9f071c797af17083cd92558fb5715fb4b
PeeterTarvas <peetertarvas@gmail.com>
2021-08-07 17:28:07 +0300

    message component is in progress

--- src/components/ProjectsPanel/Project.jsx
+++ src/components/ProjectsPanel/Project.jsx
@@ -39,7 +39,7 @@ export class Project extends React.Component{
                         <li><a href={this.props.link}>Link to projects main page</a></li>
                         <li><a href={this.props.secondLink}>{this.props.secondLink !== null ? 'Link to projects repo': ''}</a></li>
                         {this.props.imgPath !== null  ? <li><img src={this.props.imgPath}/></li>: ''}
-                        {this.props.vidURL !== null ? <li className={'vid'}><ReactPlayer url={this.props.vidURL}/></li>: ''}
+                        {this.props.vidURL !== null ? <li id={'vid'}><ReactPlayer url={this.props.vidURL}/></li>: ''}
                         <li>{this.props.shortDescription}</li>
                     </ul>
                     ): (
