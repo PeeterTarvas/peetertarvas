42e16bac510cd3581cdab6ad1c23b03320f611e4
PeeterTarvas <73399851+PeeterTarvas@users.noreply.github.com>
2021-08-09 10:01:58 +0300

    Update Information.jsx
    Typos

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -38,9 +38,10 @@ export class Information extends React.Component {
 
                     <div className={'bioHeader'} style={{fontSize:'200%', alignSelf:'flex-start'}}>Biography</div>
                     <div className={'biography'}>
-                        Hi, My name is Peeter Tarvas and I'm a second year CS student at TalTech.
-                        Im 21 years old and heave been living in Tallinn form my whole life.
-                        I aspire to become a good programmer, armature historian, designer, polymath and a decent human being.
+                        Hi, My name is Peeter Tarvas and I'm a first
+ year CS student at TalTech.
+                        Im 21 years old and have been living in Tallinn form my whole life.
+                        I aspire to become a good programmer, amature historian, designer, polymath and a decent human being.
                     </div>
                 </div>
             </div>
