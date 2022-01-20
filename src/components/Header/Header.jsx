fd36221a2e750adb35bc0695f5f3c28fc63fbd53
peeter <peetertarvas@gmail.com>
2022-01-20 15:46:00 +0200

    some changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,21 +2,14 @@ import React, {useEffect, useState} from "react";
 import {Link} from 'react-scroll'
 
 
-import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SidebarHeader,
-    SidebarContent,
-} from "react-pro-sidebar";
+
 
 
 //import icons from react icons
-import { FiGithub, FiLinkedin, FiFacebook, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
-import { SiGmail } from "react-icons/si";
+import {FiGithub, FiLinkedin, FiFacebook, FiArrowLeftCircle, FiArrowRightCircle, FiInstagram} from "react-icons/fi";
+import {IoIosArrowDropdown} from "react-icons/io"
 
 import PropTypes from 'prop-types'
-
 import "react-pro-sidebar/dist/css/styles.css";
 import "./Header.css"
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
@@ -25,46 +18,52 @@ export const Header = ({isSubmitBoxOpen}) => {
 
 
 
-    const [menuCollapse, setMenuCollapse] = useState(true)
-
-
-
-    const [scrolled, setScrolled] = useState(false)
-
-    const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 200) {
-            setScrolled(true)
-        } else {
-            setScrolled(false)
-        }
+    const [show, setShow] = useState(false);
+    const showDropdown = (e)=>{
+        setShow(!show);
+    }
+    const hideDropdown = e => {
+        setShow(false);
     }
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-    })
 
+
+    const title = () => {
+        return show ? <div style={{display: "inline-block"}}>Media <IoIosArrowDropdown/>  </div> :
+            <div style={{display: "inline-block"}}> Media <IoIosArrowDropdown id={'arrow'}/></div>
+    }
 
 
 
     return (
         <Navbar className={'navbar w-100'}>
-            <Container className={'d-flex w-50 align-items-end'} >
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto  my-lg-0"
-                     style={{ maxHeight: '100px' }}
-                     navbarScroll>
-                    <NavDropdown title={<span className="text-white w-30">Media</span>} id="basic-nav-dropdown">
-                        <NavDropdown.Item href="https://www.facebook.com/peeter.tarvas/">Facebook</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="https://www.instagram.com/ptr.trvs/">Instagram</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="https://github.com/PeeterTarvas">Github</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="https://www.linkedin.com/in/peeter-tarvas-690a58171/">Linkedin</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
+            <Container className={'d-flex flex-row w-100'} >
+                <Navbar.Collapse className="basic-navbar-nav d-flex justify-content-end">
+                    <Nav className="me-auto ">
+                        <NavDropdown show={show}
+                                     onMouseEnter={showDropdown}
+                                     onMouseLeave={hideDropdown}
+                                     title={title()}
+                                     color={'white'}
+                                     id={'dropdown'}
+                        >
+
+                            <NavDropdown.Item href="https://www.facebook.com/peeter.tarvas/" className={'d-flex justify-content-start'}>
+                                    <FiFacebook size={40}/>  <span className={'d-flex align-items-center m-2'}> Facebook</span>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="https://www.instagram.com/ptr.trvs/" className={'d-flex justify-content-start ml-2'}>
+                                    <FiInstagram size={40}/> <span className={'d-flex align-items-center m-2'}> Instagram</span>
+
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="https://github.com/PeeterTarvas" className={'d-flex justify-content-start'}>
+                                    <FiGithub size={40}/><span className={'d-flex align-items-center m-2'}> Github</span>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="https://www.linkedin.com/in/peeter-tarvas-690a58171/" className={'d-flex justify-content-start'}>
+                                    <FiLinkedin size={40}/><span className={'d-flex align-items-center m-2'}> Linkedin</span>
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
 
